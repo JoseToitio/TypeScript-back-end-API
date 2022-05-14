d75f9a796f26db72ac82dbdc2717698555280359
JoseToitio <josetoitio@gmail.com>
2022-05-14 05:05:53 -0300

    adiciona rota post users

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import ProductRoute from './routes/product.routes';
+import UserRoute from './routes/user.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(ProductRoute);
+app.use(UserRoute);
 
 export default app;
